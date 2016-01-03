99a92cf4af6f4ba41d58a09e9d8d1b1c5cd48974
fuimaz <fujiman760@gmail.com>
2016-01-03 19:54:48 +0800

    first commit

---
+++ MyJsonPractice/src/base_pra.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <string>
+
+#include <json.h>
+
+using namespace std;
+
+int main()
+{
+	Json::Reader jreader;
+	string	value("{\"uploadid\": \"UP000000\",\"code\": 100,\"msg\": \"\",\"files\": \"\"}");
+	Json::Value	jvalue;
+	
+	if (jreader.parse(value, jvalue))
+	{
+		string upload_id = jvalue["upload_id"].asString();
+		int code = jvalue["code"].asInt();
+		cout << upload_id << code << endl;
+	}
+
+	return 0;
+}
